55b5616fb92b26cad284f42441a62c0fda5fad53
Michael Stuart <mike.w.stuart@gmail.com>
2016-09-27 21:39:44 -0700

    change default port

--- app.js
+++ app.js
@@ -76,6 +76,6 @@ app.get('/campgrounds/:id', function(req, res) {
     });
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, function(){
     console.log('serves up');
 });
