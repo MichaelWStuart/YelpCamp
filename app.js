b29b39ede1176525f43aa620b35119d6593992ed
Michael Stuart <mike.w.stuart@gmail.com>
2016-10-04 18:27:17 -0700

    266. Add register form/routes

--- app.js
+++ app.js
@@ -15,6 +15,18 @@ app.set('view engine','ejs');
 app.use(express.static(__dirname + '/public'));
 seedDB();
 
+//PASSPORT CONFIGURATION
+app.use(require('express-session')({
+  secret: 'mellon',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 //DEFAULT
 app.get('/', function(req, res){
   res.render('landing');
@@ -97,6 +109,31 @@ app.post('/campgrounds/:id/comments', function(req, res){
       }
     });
 });
+
+//===========
+//AUTH ROUTES
+//==========
+
+//show register form
+app.get('/register', function(req, res){
+  res.render('register');
+});
+
+//handle sign up logic
+app.post('/register', function(req, res){
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user){
+    if (err){
+      console.log(err);
+      return res.render('register');
+    }
+    passport.authenticate('local')(req, res, function(){
+      res.redirect('/campgrounds');
+    });
+  });
+});
+
+
 
 app.listen(3000, function(){
     console.log("serve's up");
