73dfe34b33111b13e46ea89bd76dc046bd4b70b5
Michael Stuart <mike.w.stuart@gmail.com>
2016-10-04 19:39:24 -0700

    add logout route/auth middleware, fix navbar links

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.get('/campgrounds/:id', function(req, res) {
 });
 
 //COMMENTS
-app.get('/campgrounds/:id/comments/new', function(req, res){
+app.get('/campgrounds/:id/comments/new', isLoggedIn, function(req, res){
   Campground.findById(req.params.id, function(err, campground){
     if (err) {
       console.log(err);
@@ -145,6 +145,19 @@ app.post('/login', passport.authenticate('local',
     failureRedirect: '/login'
   }), function(req, res){
 });
+
+//logout route
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/campgrounds');
+});
+
+function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/login');
+}
 
 app.listen(3000, function(){
     console.log("serve's up");
